596641551f0e7d6943ef59ad81e31a0218348f3a
Tomás de Azkue <tomasdzk@gmail.com>
2023-11-27 20:02:23 -0300

    Tomás
    Correción de footer, botones y cards de Productos

--- eccomerce/src/page/productos.jsx
+++ eccomerce/src/page/productos.jsx
@@ -4,23 +4,29 @@ import TarjetaProducto from '../component/TarjetaProducto';
 
 const Productos = () => {
   const listaDeArticulos = [
-    { id: 1, nombre: 'Remera', precio: '$10' },
-    { id: 2, nombre: 'Pantalon', precio: '$20' },
-    { id: 3, nombre: 'Pullover', precio: '$30' },
+    { id: 1, nombre: 'Remeras', precio: '$10' },
+    { id: 2, nombre: 'Gorras', precio: '$20' },
+    { id: 3, nombre: 'Pantalones', precio: '$30' },
     
-   
+  
   ];
 
   return (
-    <>
-      <h1 className="text-8xl text-center align-middle font-serif m-0 p-0 italic">Productos</h1>
-      <div className="ml-20 mt-10 grid grid-flow-col justify-stretch m-0 p-20">
+    <><div style={{minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingBottom: '20%'}}>
+      <h1 className="text-8xl font-serif mb-8 italic">Productos</h1>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="flex justify-center">
         {listaDeArticulos.map((articulo) => (
-          <TarjetaProducto key={articulo.id} articulo={articulo} />
+        <div key={articulo.id} className='mb-8 mx-64'>
+          <TarjetaProducto articulo={articulo}/>   
+        </div> 
         ))}
       </div>
-
-   
+    </div>
     </>
   );
 };
